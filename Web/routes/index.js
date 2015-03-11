d90e1ceff83db41b5383679b5c8d1d9d237909cb
Mark Peterson <mark.peterson@live.com>
2015-03-10 22:10:16 -0700

    Fixed deployment to ElasticBeanstalk
    In order for our package to deploy to EB the following must be true:
    1) package.json must include the scripts/start value of "node ./bin/www"
    to launch the app
    2) The main app cannot be called "app.js", so it's currently renamed to
    "main.js"

--- Web/routes/index.js
+++ Web/routes/index.js
@@ -1,19 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var io = require('socket.io')(http);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'VineCache' });
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-    socket.on('push notification', function(msg){
-    io.emit('push notification', msg);
-  });
 });
 
 module.exports = router;
