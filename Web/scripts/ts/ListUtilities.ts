d559bcdaf25c09ba8b8fe5357c65e39584aaa5e8
caromu <caro_brown@outlook.com>
2015-03-12 10:00:48 -0700

    Hookup event list to real data from parse.com

--- Web/scripts/ts/ListUtilities.ts
+++ Web/scripts/ts/ListUtilities.ts
@@ -20,7 +20,7 @@ module ListUtilities {
 
     class EventListItemProvider implements ListItemProvider {
         constructor() {
-            Parse.initialize("ODbBwcIu8uZ4zuJ8PGsinEtXeyUswCXL9pUnddov", "uVOwgMNsmE8l2D47IiM179B3RLJnJdRGKAWZvR5j");
+            Parse.initialize("ODbBwcIu8uZ4zuJ8PGsinEtXeyUswCXL9pUnddov", "H9tKhwb9aVps6QOxRYiG8NHEpXZdHK8Qlk6W8nF5");
         }
         addItems(list: HTMLUListElement) {
             this.queryEvents().then((eventNames: string[]) => {
@@ -29,21 +29,20 @@ module ListUtilities {
         }
 
         private queryEvents() {
-            //var Event = Parse.Object.extend("Event");
-            //var query = new Parse.Query(Event);
-            //return query.find().then(
-            //	(results: any[]) => {
-            //		var eventNames: string[] = [];
-            //		if (results && results.length > 0) {
-            //			results.forEach((object: Parse.Object) => { eventNames.push(object.get("Name")); });
-            //		}
-            //		return Parse.Promise.as(eventNames);
-            //	},
-            //	(reason: Parse.Error) => {
-            //		alert(reason.message);
-            //		return Parse.Promise.error(reason);
-            //	});
-            return Parse.Promise.as(["First", "Second", "Third"]);
+            var Event = Parse.Object.extend("Event");
+            var query = new Parse.Query(Event);
+            return query.find().then(
+                (results: any[]) => {
+                    var eventNames: string[] = [];
+                    if (results && results.length > 0) {
+                        results.forEach((object: Parse.Object) => { eventNames.push(object.get("Name")); });
+                    }
+                    return Parse.Promise.as(eventNames);
+                },
+                (reason: Parse.Error) => {
+                    alert(reason.message);
+                    return Parse.Promise.error(reason);
+                });
         }
 
         private buildItem(name: string) {
