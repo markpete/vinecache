46503d230acc508dfba75b0a363cbc314c23b95a
Mark Peterson <mark.peterson@live.com>
2015-03-13 08:46:39 -0700

    Event fix

--- Web/scripts/ts/Event.ts
+++ Web/scripts/ts/Event.ts
@@ -1,41 +1,44 @@
 ﻿///<reference path="../types/node/node.d.ts" />
 ///<reference path="../types/googleMaps/googleMaps.d.ts" />
+(function () {
+    if (document.readyState === "complete") {
+        initPage();
+    } else {
+        document.addEventListener("DOMContentLoaded",(ev: Event) => {
+            initPage();
+        });
+    }
 
-if (document.readyState === "complete") {
-    initPageNew();
-} else {
-    document.addEventListener("DOMContentLoaded",(ev: Event) => {
-        initPageNew();
-    });
-}
+    function initPage() {
+        var headerContainer = document.createElement("div");
+        headerContainer.className = "page-header";
+        document.body.appendChild(headerContainer);
 
-function initPageNew() {
-    var eventInfo = JSON.parse(sessionStorage.getItem("currentEvent"));
-    var title = (eventInfo && eventInfo.Name || "Event")
+        var eventName = sessionStorage.getItem("currentEvent") || "Event";
+        var title = eventName;
 
-    GlobalNav.GlobalNavBuilder.buildNav();
-    var header = document.createElement("h1");
-    header.innerText = title;
-    document.body.appendChild(header);
-    
-    var description = document.createElement("p");
-    description.innerText = "Welcome to " + title;
-    document.body.appendChild(description);
+        GlobalNav.GlobalNavBuilder.buildNav(headerContainer);
 
-    var mapDiv = document.createElement("div");
-    mapDiv.id = "map-canvas";
-    document.body.appendChild(mapDiv);
-    (new MapModule.Map()).initializeMap(); 
-    //window.addEventListener('load', function () {
-    //    if (document.getElementById('map')) {
+        var header = document.createElement("h2");
+        header.innerText = title;
+        header.style.color = "#4E9A06";
+        document.body.appendChild(header);
 
-    //                new google.maps.Map(document.getElementById('map'), {
-    //                    center: new google.maps.LatLng(0, 0),
-    //                    zoom: 3
-    //                });
+        var mapDiv = document.createElement("div");
+        mapDiv.id = "map-canvas";
+        document.body.appendChild(mapDiv);
+        (new MapModule.Map()).initializeMap(); 
+        //window.addEventListener('load', function () {
+        //    if (document.getElementById('map')) {
+
+        //                new google.maps.Map(document.getElementById('map'), {
+        //                    center: new google.maps.LatLng(0, 0),
+        //                    zoom: 3
+        //                });
          
-    //    }
-    //}, false);
-    // 
-}
+        //    }
+        //}, false);
+        // 
+    }
+})();
 
