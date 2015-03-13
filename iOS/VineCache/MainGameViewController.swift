053f11951016fc334b4a1fac16ff5e50fc9d13d2
Yossi Katzin <joseph.katzin@gmail.com>
2015-03-13 10:43:54 -0700

    Proximity bar
    I have it mostly set up. Untested, though.

--- iOS/VineCache/MainGameViewController.swift
+++ iOS/VineCache/MainGameViewController.swift
@@ -9,23 +9,65 @@
 import UIKit
 import CoreLocation
 
-class MainGameViewController: UIViewController {
+class MainGameViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet var proximityBar:UIProgressView!
+    
+    var locationManager:CLLocationManager!
+    var currentLocation:CLLocation!
+    var destination:CLLocation!
+    var distanceInitialToDestination:CLLocationDistance!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.desiredAccuracy = 10
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
 
     @IBAction func RecordButtonClick(sender: UIButton) {
         self.performSegueWithIdentifier("segueIdentifier", sender: self)
     }
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as CLLocation
+        currentLocation = location
+        newDestination()
+        updateProgress()
+    }
+    
+    func newDestination(destination:CLLocation? = nil) {
+        if destination != nil {
+            self.destination = destination
+        }
+        if self.destination != nil && currentLocation != nil {
+            distanceInitialToDestination = currentLocation.distanceFromLocation(self.destination)
+        }
+    }
+    
+    func updateProgress() {
+        if destination == nil {
+            return
+        }
+        let distance = currentLocation.distanceFromLocation(destination)
+        var ratio:Float = (Float)(distance as Double / distanceInitialToDestination as Double)
+        ratio /= 2
+        proximityBar.setProgress(ratio, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
